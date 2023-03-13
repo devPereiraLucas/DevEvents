861091b8329169a5188f00e41fae294b3e07c1fb
Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-03-12 23:11:35 -0300

    criando persistencia de dados local

---
+++ DevEvents.API/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DevEvents.API.Entities;
+
+namespace DevEvents.API.Persistence
+{
+    public class DevEventsDbContext
+    {
+        public List<DevEvent> DevEvents { get; set; }
+        public DevEventsDbContext()
+        {
+            DevEvents = new List<DevEvent>();
+        }
+    }
+}
