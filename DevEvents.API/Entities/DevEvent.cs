6785b2c538f431e66f1fdf85e2b31b246e27875d
Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-03-12 23:11:15 -0300

    criando entidades para o projeto

--- DevEvents.API/Entities/DevEvent.cs
+++ DevEvents.API/Entities/DevEvent.cs
@@ -2,5 +2,31 @@
 {
     public class DevEvent
     {
+        public DevEvent()
+        {
+            Speakers = new List<DevEventSpeaker>();
+            isDeleted = false;
+        }
+
+        public Guid Id { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<DevEventSpeaker> Speakers { get; set; }
+        public bool isDeleted { get; set; }
+
+        public void Update(String title, String description, DateTime startDate, DateTime endDate) 
+        {
+            Title = title;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public void Delete()
+        {
+            isDeleted = true;
+        }
     }
 }
---
+++ DevEvents.API/Entities/DevEventSpeaker.cs
@@ -0,0 +1,11 @@
+﻿namespace DevEvents.API.Entities
+{
+    public class DevEventSpeaker
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String TalkTitle { get; set; }
+        public String TalkDescription { get; set; }
+        public String LinkedInProfile { get; set; }
+    }
+}
